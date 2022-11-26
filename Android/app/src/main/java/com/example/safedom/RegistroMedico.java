1e2371b9d22655c7b378191568cef61d5a83c7a5
Carlos Botella <carlosb.n@icloud.com>
2022-11-26 16:08:16 +0100

    Añadir genero medico correcto
    Ya se puede añadir el genero del médico pero no sé por que no funciona añadir el género del paciente

--- Android/app/src/main/java/com/example/safedom/RegistroMedico.java
+++ Android/app/src/main/java/com/example/safedom/RegistroMedico.java
@@ -2,10 +2,16 @@ package com.example.safedom;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +22,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.Objects;
 
 public class RegistroMedico extends AppCompatActivity {
+    private String genero = "";
     private String nombre = "";
     private String apellido = "";
     private String correo = "";
@@ -23,39 +30,65 @@ public class RegistroMedico extends AppCompatActivity {
     private String ccontraseña = "";
     private String idmedico = "";
     private String rol = "Medico";//Paciente
-    private EditText etCorreo, etContraseña, etNombre, etApellido, etCcontraseña,etIdMedico;
-    private TextInputLayout tilCorreo, tilContraseña, tilCcontraseña, tilNombre, tilApellido,tilIdMedico;
+    private EditText etCorreo, etContraseña, etNombre, etApellido, etCcontraseña, etIdMedico;
+    private TextInputLayout tilCorreo, tilContraseña, tilCcontraseña, tilNombre, tilApellido, tilIdMedico, tilGenero;
     Button bs;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private FirebaseAuth mAuth= FirebaseAuth.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    private static final String[] generoLista = {"Hombre", "Mujer", "No Binario"};
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.registro_medico);
         etCorreo = (EditText) findViewById(R.id.Clec);
         etContraseña = (EditText) findViewById(R.id.Pass);
         etCcontraseña = (EditText) findViewById(R.id.Repass);
         etNombre = (EditText) findViewById(R.id.Nombre);
         etApellido = (EditText) findViewById(R.id.Apellido);
-        etIdMedico =(EditText) findViewById(R.id.id_medico);
+        etIdMedico = (EditText) findViewById(R.id.id_medico);
         tilCorreo = (TextInputLayout) findViewById(R.id.til_Clec);
         tilContraseña = (TextInputLayout) findViewById(R.id.til_Pass);
         tilCcontraseña = (TextInputLayout) findViewById(R.id.til_Repass);
         tilNombre = (TextInputLayout) findViewById(R.id.til_nombre);
         tilApellido = (TextInputLayout) findViewById(R.id.til_apellido);
         tilIdMedico = (TextInputLayout) findViewById(R.id.til_id_medico);
+        tilGenero = (TextInputLayout) findViewById(R.id.spinner_genero);
         bs = (Button) findViewById(R.id.finalizar);
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autocomplete);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(RegistroMedico.this,
+                android.R.layout.simple_spinner_dropdown_item, generoLista);
+        autoCompleteTextView.setAdapter(adapter);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Toast.makeText(RegistroMedico.this, autoCompleteTextView.getText().toString(), Toast.LENGTH_SHORT).show();
+                if (autoCompleteTextView.getText().toString().equals("Hombre")) {
+                    genero = generoLista[0];
+                } else if (autoCompleteTextView.getText().toString().equals("Mujer")) {
+                    genero = generoLista[1];
+                } else if (autoCompleteTextView.getText().toString().equals("No Binario")) {
+                    genero = generoLista[2];
+                }
+            }
+        });
+//        spinner.setOnItemSelectedListener(this);
         bs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(Validar()){
-                    db.collection("Users").document(correo).set(new Medico(correo,contraseña,nombre,apellido,rol,idmedico));
+                if (Validar()) {
+                    db.collection("Users").document(correo).set(new Medico(correo, contraseña, nombre, apellido, rol, idmedico, genero));
                     mAuth.createUserWithEmailAndPassword(correo, ccontraseña);
 
-                    startActivity(new Intent(RegistroMedico.this,CustomLoginActivity.class));
+                    startActivity(new Intent(RegistroMedico.this, CustomLoginActivity.class));
                 }
             }
         });
     }
+
+
+
     public boolean Validar() {
         boolean s = true;
         correo = etCorreo.getText().toString();
@@ -63,7 +96,7 @@ public class RegistroMedico extends AppCompatActivity {
         ccontraseña = etCcontraseña.getText().toString();
         nombre = etNombre.getText().toString();
         apellido = etApellido.getText().toString();
-        idmedico=etIdMedico.getText().toString();
+        idmedico = etIdMedico.getText().toString();
         if (correo.isEmpty()) {
             etCorreo.setError("Este campo no puede estar vacio");
             s = false;
@@ -74,7 +107,7 @@ public class RegistroMedico extends AppCompatActivity {
         if (contraseña.isEmpty()) {
             etContraseña.setError("Este campo no puede estar vacio");
             s = false;
-        }else if (contraseña.length() < 8) {
+        } else if (contraseña.length() < 8) {
             etContraseña.setError("La contraseña ha de contener al menos 8 caracteres");
             s = false;
         }
@@ -90,11 +123,16 @@ public class RegistroMedico extends AppCompatActivity {
         if (ccontraseña.isEmpty()) {
             etCcontraseña.setError("Este campo no puede estar vacio");
             s = false;
-        }else if (!Objects.equals(ccontraseña, contraseña)) {
+        } else if (!Objects.equals(ccontraseña, contraseña)) {
             etCcontraseña.setError("No coincide con la contraseña");
             s = false;
-        }if (idmedico.isEmpty()) {
+        }
+        if (idmedico.isEmpty()) {
             etIdMedico.setError("Este campo no puede estar vacio");
+            s = false;
+        }
+        if (genero.equals("")) {
+            tilGenero.setError("Por favor elija su genero");
             s = false;
         }
         return s;
