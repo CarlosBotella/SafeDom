967502e6a88576854c95abe414b101e054b44611
Carlos Botella <carlosb.n@icloud.com>
2022-12-09 17:48:51 +0100

    llamar por tlf paciente

--- Android/app/src/main/java/com/example/safedom/PacienteP/InfoPaciente.java
+++ Android/app/src/main/java/com/example/safedom/PacienteP/InfoPaciente.java
@@ -1,6 +1,12 @@
 package com.example.safedom.PacienteP;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,28 +15,20 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import com.example.safedom.AdminP.VistaAdmin;
-import com.example.safedom.CasaP.CrearCasa;
 import com.example.safedom.R;
 import com.example.safedom.clases.User;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.Objects;
 
 
 public class InfoPaciente extends AppCompatActivity {
     private ImageView imginfo;
-    private TextView nombreinfo, correoinfo,telefonoinfo,generoinfo,dobinfo,alturainfo,pesoinfo;
+    private TextView nombreinfo, correoinfo, telefonoinfo, generoinfo, dobinfo, alturainfo, pesoinfo;
     private User user;
     Button be;
-    String id="";
-    String rol="";
+    String id = "";
+    String rol = "";
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
@@ -43,6 +41,33 @@ public class InfoPaciente extends AppCompatActivity {
         initValues();
 
 
+    }
+
+    public void llamarTelefono(View view) {
+        Log.e("PRUEBAS: ", "llamarTelefono: OUT");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            Log.e("PRUEBAS", "llamarTelefono: PERMISSION ACC");
+            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + telefonoinfo.getText().toString()));
+            startActivity(intent);
+        } else {
+            Log.e("PRUEBAS", "llamarTelefono: PERMISSION DEN " );
+            solicitarPermiso(Manifest.permission.CALL_PHONE, "Sin el permiso de llamada no se puede realizar la llamada", 1, this);
+        }
+
+    }
+
+    private void solicitarPermiso(String permiso, String justificacion, final int requestCode, final Activity actividad) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(actividad, permiso)) {
+            new AlertDialog.Builder(actividad)
+                    .setTitle("Solicitud de permiso")
+                    .setMessage(justificacion)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            ActivityCompat.requestPermissions(actividad, new String[]{permiso}, requestCode);}}).show();
+        } else {
+            // No se necesita mostrar la explicación al usuario, solicitar el permiso
+            ActivityCompat.requestPermissions(actividad, new String[]{permiso}, requestCode);
+        }
     }
 
     private void initViews() {
@@ -60,18 +85,19 @@ public class InfoPaciente extends AppCompatActivity {
 
                                           });*/
 
-        imginfo=findViewById(R.id.infoimg);
-        nombreinfo=findViewById(R.id.infonombre);
-        correoinfo=findViewById(R.id.infoacorreo);
-                telefonoinfo=findViewById(R.id.infotelefono);
-        generoinfo=findViewById(R.id.infogenero);
-                dobinfo=findViewById(R.id.infodb);
-        alturainfo=findViewById(R.id.infaltura);
-                pesoinfo=findViewById(R.id.infopeso);
+        imginfo = findViewById(R.id.infoimg);
+        nombreinfo = findViewById(R.id.infonombre);
+        correoinfo = findViewById(R.id.infoacorreo);
+        telefonoinfo = findViewById(R.id.infotelefono);
+        generoinfo = findViewById(R.id.infogenero);
+        dobinfo = findViewById(R.id.infodb);
+        alturainfo = findViewById(R.id.infaltura);
+        pesoinfo = findViewById(R.id.infopeso);
     }
+
     private void initValues() {
-        user=(User) getIntent().getExtras().getSerializable("pacienteInfo");
-        nombreinfo.setText(user.getNombre() + " " +user.getApellido());
+        user = (User) getIntent().getExtras().getSerializable("pacienteInfo");
+        nombreinfo.setText(user.getNombre() + " " + user.getApellido());
         correoinfo.setText(user.getUserEmail());
         telefonoinfo.setText(user.getTelefono());
         generoinfo.setText(user.getGenero());
